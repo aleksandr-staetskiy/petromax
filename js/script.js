4ecef914f8a6e53ba859076e05c0aabc384c441a
Aleksandr Staetsky <myexploretime@gmail.com>
2019-09-20 16:46:23 +0300

    load more logic

--- js/script.js
+++ js/script.js
@@ -12,4 +12,20 @@ $('a[href*="#"]').on('click', function(e) {
   )
 })
 
+
+$( document ).ready(function () {
+
+  $(".moreBox").slice(0, 3).show();
+  if ($(".blogBox:hidden").length != 0) {
+    $("#loadMore").show();
+  }		
+  $("#loadMore").on('click', function (e) {
+    e.preventDefault();
+    $(".moreBox:hidden").slice(0, 6).slideDown();
+    if ($(".moreBox:hidden").length == 0) {
+      $("#loadMore").fadeOut('slow');
+    }
+  });
+});
+
 AOS.init();
